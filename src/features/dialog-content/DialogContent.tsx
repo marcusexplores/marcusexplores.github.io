e5e0353c369f804ddd8ee18bc56dcf6ecd8213c4
Marcus Ng <marcusngwj@msn.com>
2025-09-25 22:11:36 +0800

    remove close button

--- src/features/dialog-content/DialogContent.tsx
+++ src/features/dialog-content/DialogContent.tsx
@@ -1,5 +1,4 @@
-import { Content, Close } from "@radix-ui/react-dialog";
-import { XIcon } from "lucide-react";
+import { Content } from "@radix-ui/react-dialog";
 import { DialogPortal } from "../dialog-portal/DialogPortal";
 import { DialogOverlay } from "../dialog-overlay/DialogOverlay";
 import { cn } from "../../functions/cn";
@@ -21,10 +20,6 @@ export const DialogContent = ({
         {...props}
       >
         {children}
-        <Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4">
-          <XIcon />
-          <span className="sr-only">Close</span>
-        </Close>
       </Content>
     </DialogPortal>
   );
