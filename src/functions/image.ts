6a15ba6e3e0e4a8c1606cdbcfeac8951cf646222
Marcus Ng Wen Jian <13817632+marcusngwenjian@users.noreply.github.com>
2025-10-04 15:00:35 +0800

    Update image path resolution

---
+++ src/functions/image.ts
@@ -0,0 +1,16 @@
+const SOURCE_PATH_ALIAS_SYMBOL = "@"
+const SOURCE_PATH_RELATIVE_SYMBOL = ".."
+
+const imageModules = import.meta.glob('../assets/**/*.{jpg,jpeg,png,webp}', {
+  eager: true,
+  import: 'default',
+});
+
+export const resolveImagePath = (path: string) => {
+  const isExternal = path?.startsWith('https://');
+
+  // Replace the alias with the relative path
+  const key = `${SOURCE_PATH_RELATIVE_SYMBOL}${path.substring(SOURCE_PATH_ALIAS_SYMBOL.length)}`;
+
+  return isExternal ? path : imageModules[key] as string | undefined;
+}
