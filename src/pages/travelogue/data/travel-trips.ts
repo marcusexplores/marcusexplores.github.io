6bde5b64d21adbe351ac90138bfb9e5ce2bab3f3
Marcus Ng <marcusngwj@msn.com>
2025-10-05 15:09:42 +0800

    add content to summary

--- src/pages/travelogue/data/travel-trips.ts
+++ src/pages/travelogue/data/travel-trips.ts
@@ -14,7 +14,7 @@ export const travelTrips: DeepReadonly<TravelTrip[]> = [
     region: "Taiwan",
     startDate: "2025-01-05",
     endDate: "2025-01-14",
-    description: "Good trip",
+    description: "An ultimate 10-day Taiwanese escape, chasing vibrant culture and savoring mouthwatering flavors while exploring iconic landmarks like Taipei 101 and the serene bay of Tamsui.",
     imageUrl: "@/assets/travelogue/dream-around-taiwan.jpg",
     website: "https://sites.google.com/view/dream-around-taiwan"
   },
@@ -23,7 +23,7 @@ export const travelTrips: DeepReadonly<TravelTrip[]> = [
     region: "Japan",
     startDate: "2024-09-07",
     endDate: "2024-09-15",
-    description: "Good trip",
+    description: "A captivating summer journey through Tokyo, immersing in a world of dreams and magic with a fully mapped-out itinerary and detailed expenditure planning.",
     imageUrl: "@/assets/travelogue/tokyo-summer-cats.jpg",
     website: "https://sites.google.com/view/tokyo-summer-cats"
   },
@@ -32,7 +32,7 @@ export const travelTrips: DeepReadonly<TravelTrip[]> = [
     region: "Japan",
     startDate: "2024-03-22",
     endDate: "2024-04-03",
-    description: "Good trip",
+    description: "Captivating adventure chasing the Sakura across Tokyo, Osaka, and their neighboring areas, diving into vibrant anime culture and savoring exquisite cuisine.",
     imageUrl: "@/assets/travelogue/the-sakura-chase.jpg",
     website: "https://sites.google.com/view/the-sakura-chase"
   },
@@ -41,7 +41,7 @@ export const travelTrips: DeepReadonly<TravelTrip[]> = [
     region: "Indonesia",
     startDate: "2024-01-01",
     endDate: "2024-01-01",
-    description: "Good trip",
+    description: "Weekend escape to Batam, featuring a detailed travel itinerary and a gallery of captured moments from the Indonesian island getaway.",
     imageUrl: "@/assets/travelogue/batam-escapade.jpg",
     website: "https://sites.google.com/view/batam-escapade"
   },
@@ -50,7 +50,7 @@ export const travelTrips: DeepReadonly<TravelTrip[]> = [
     region: "China",
     startDate: "2023-12-08",
     endDate: "2023-12-13",
-    description: "Good trip",
+    description: "Journey through Chengdu to witness the enchanting Giant Pandas, explore ancient cultural treasures like the Wuhou Shrine, and indulge in the diverse, exquisite flavors of authentic Sichuan cuisine.",
     imageUrl: "@/assets/travelogue/chengdu-for-you.jpg",
     website: "https://sites.google.com/view/chengdu-for-you"
   },
@@ -59,7 +59,7 @@ export const travelTrips: DeepReadonly<TravelTrip[]> = [
     region: "Japan",
     startDate: "2023-03-19",
     endDate: "2023-04-02",
-    description: "Good trip",
+    description: "Chasing the first cherry blossoms across Japan, experiencing the magic of Hanami in Tokyo and Kyoto alongside vibrant cultural traditions and ancient temples.",
     imageUrl: "@/assets/travelogue/march-to-japan.jpg",
     website: "https://marchtojapan.web.app"
   }
