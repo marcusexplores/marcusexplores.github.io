979c13b70bc0ce6228fbcb098882991c69d0e164
Marcus Ng <marcusngwj@msn.com>
2025-10-16 23:39:11 +0800

    update logic of redirect url to external site

--- src/pages/not-found/NotFoundPage.tsx
+++ src/pages/not-found/NotFoundPage.tsx
@@ -2,25 +2,14 @@ import { Link, useLocation } from "react-router";
 import { Button } from "@/components/button/Button";
 import { NAVIGATION_KEY } from "@/components/navigation/constants";
 
-const transformPath = (input: string) => {
-  const pattern1 = /^\/#\/(.*?)\/(.*)$/;
-  const match1 = pattern1.exec(input);
-  if (match1) {
-    return `/${match1[1]}/#/${match1[2]}`;
-  }
-
-  const pattern2 = /^\/#\/(.*)$/;
-  const match2 = pattern2.exec(input);
-  if (match2) {
-    return `/${match2[1]}`;
-  }
-
-  return input;
-}
-
 export const NotFoundPage = () => {
   const location = useLocation();
-  const redirectUrl = transformPath(location.pathname);
+  const domainName = window.location.hostname;
+  
+  const redirectUrl = domainName+ location.pathname.replace(new RegExp("^\\/([^\\/]+)\\/(.*)$"), '/$1/#/$2')
+
+  console.log("Path: " + location.pathname)
+  console.log("Redirect: " + redirectUrl)
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-blue-50 text-gray-800 font-sans p-4">
@@ -46,7 +35,7 @@ export const NotFoundPage = () => {
           className="px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-lg shadow-md transition duration-300 ease-in-out transform hover:scale-105"
         >
           {/* Redirect to GitHub Pages if it exist */}
-          <Link to={redirectUrl}>Redirect</Link>
+          <a href={redirectUrl}>Redirect</a>
         </Button>
       </div>
       <div className="max-w-md w-full mt-8">{/* The image will go here */}</div>
